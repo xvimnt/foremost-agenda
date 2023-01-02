300fba74584dde8750aca4b9a70277c3b247783e
xvimnt <jvmonteros98@gmail.com>
2023-01-02 15:13:50 -0600

    first version

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-12'>
+          <h1 className='text-muted'>Agenda</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
