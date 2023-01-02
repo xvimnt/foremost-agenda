0b320ea71f297c907a4fb30c067fc67166b14ed3
xvimnt <jvmonteros98@gmail.com>
2023-01-02 17:22:01 -0600

    modal working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 import ItemList from './components/ItemList';
+import Modal from './components/Modal';
+import { useState } from 'react';
+
 function App() {
+  const [ title, setTitle ] = useState('');
+  const [ body, setBody ] = useState('');
   return (
     <div className='container'>
       <div className='row'>
@@ -14,7 +19,7 @@ function App() {
         </div>
         <div className='col-6'>
           <div className="btn-group" role="group" aria-label="Basic example">
-            <button type="button" className="btn btn-dark">Add New</button>
+            <button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#add">Add New</button>
             <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
               Sort By
             </button>
@@ -27,6 +32,18 @@ function App() {
         </div>
       </div>
       <ItemList></ItemList>
+      <Modal id="add" title="Add New Note">
+        <form>
+          <div class="mb-3">
+            <label htmlFor="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+          </div>
+          <div class="mb-3">
+            <label htmlFor="body" class="form-label">Body</label>
+            <textarea rows="15" type="text" class="form-control" id="body" value={body} onChange={(e) => setBody(e.target.value)}/>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 }
--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,4 +1,6 @@
 import Item from "./Item";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const notes = [
     {
@@ -23,13 +25,42 @@ const notes = [
     },
 ]
 
-export default function ItemList({ note }) {
+export default function ItemList() {
+
+    const [ title, setTitle ] = useState('');
+    const [ body, setBody ] = useState('');
+    const [ date, setDate ] = useState('');
+
+    const click = (note) => {
+        setTitle(note.title);
+        setBody(note.body);
+        setDate(note.date);
+    }
+
     return (
-        <div class="list-group">
-            {notes.map(note => {
-                return <Item key={note.title} note={note}></Item>
-            })}
-        </div>
+        <>
+            <div className="list-group">
+                {notes.map(note => {
+                    return <Item key={note.title} note={note} click={click}></Item>
+                })}
+            </div>
+            <Modal id="edit" title="View Note">
+                <form>
+                    <div className="text-center">
+                        <p className="text-muted">{date}</p>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="title" className="form-label">Title</label>
+                        <input type="text" className="form-control" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="body" className="form-label">Body</label>
+                        <textarea rows="15" type="text" className="form-control" id="body" value={body} onChange={(e) => setBody(e.target.value)} />
+                    </div>
+                </form>
+            </Modal>
+        </>
+
     );
 }
 
